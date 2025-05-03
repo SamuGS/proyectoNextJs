5e7d312dc4df82f8b7c06736c8fa71f3dc04de2b
samugs <garcias110800@gmail.com>
2025-05-03 10:53:04 -0600

    Subiendo archivos restantes

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import {
   UserGroupIcon,
   HomeIcon,
@@ -6,6 +8,7 @@ import {
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import clsx from 'clsx';
+import { use } from 'react';
 
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
@@ -33,7 +36,9 @@ export default function NavLinks() {
             href={link.href}
             className={clsx(
               "flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3",
-              {'bg-sky-100 text-blue-600': pathname === link.href},
+              {
+                'bg-sky-100 text-blue-600': pathname === link.href,
+              },
             )}
           >
           <LinkIcon className="w-6" />
